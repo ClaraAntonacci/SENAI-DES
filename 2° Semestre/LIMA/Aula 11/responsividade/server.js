63149e27306a31ef0feff727b08a3bcc652ba798
ClaraAntonacci <andrzejewskyantonacciclara@gmail.com>
2025-10-16 17:06:42 -0300

    atualização

--- 2° Semestre/LIMA/Aula 11/responsividade/server.js
+++ 2° Semestre/LIMA/Aula 11/responsividade/server.js
@@ -1,5 +1,13 @@
 const menu = document.querySelector("#menu");
 
 menu.addEventListener("click", function(){
-    console.log("teste");
+    const nav = document.querySelector("nav");
+
+    const leftNav = nav.style.left;
+
+    if(leftNav === "" || leftNav === "-200px"){
+        nav.style.left = "0";
+    }else{
+        nav.style.left = "-200px";
+    }
 });
